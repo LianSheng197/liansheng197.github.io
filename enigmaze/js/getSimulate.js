70179f7d5b6cf8fba6c3a7e494b400f974b3c0fe
LianSheng197 <ci.i.cxcvii@gmail.com>
2018-07-24 13:38:41 +0800

    $_GET['param'] simulate.

--- enigmaze/js/getSimulate.js
+++ enigmaze/js/getSimulate.js
@@ -1,2 +1,8 @@
 var url = window.location.href; 
 var pw = url.match(/\?(.*$)/)[1];
+
+// $_GET['name'] simulate.
+const $_GET = (search) => {
+    var re = new RegExp(search+"=(.*?)(&|$)")
+    return pw.match(re)[1]
+}
