3fbfa0fed61a902f519f99d19f767ed59e337a7f
LianSheng197 <ci.i.cxcvii@gmail.com>
2018-12-03 12:20:42 +0800

    Kabuntu (Preview: 4)
    新增了視窗開啟功能 (由側欄開啟，除了搜尋)
    新增了 nautilus, trush, setting 視窗，但預設隱藏
    修正了視窗預設樣式及位置
    修正了導航欄樣式問題
    修正了右上時間的更新問題
    移除了因網頁上傳更改過路徑之檔案的造成的重複檔案

--- kabuntu/js/nav_time.js
+++
@@ -1,9 +0,0 @@
-function now(){
-    var now = new Date();
-    var hh = (now.getHours() < 10)? "0" + now.getHours() : now.getHours();
-    var mm = (now.getMinutes() < 10)? "0" + now.getMinutes() : now.getMinutes();
-    var ss = (now.getSeconds() < 10)? "0" + now.getSeconds() : now.getSeconds();
-    var time = hh + ":" + mm + ":" + ss;
-
-    document.getElementById("time").innerText = time;
-}
--- kabuntu/js/window.js
+++ kabuntu/js/window.js
@@ -5,9 +5,9 @@ var title = document.getElementById("title");
 var desktop = document.getElementById("main");
 var terminal = document.getElementById("window1");
 var firefox = document.getElementById("window2");
-// var nautilus = document.getElementById("window3");
-// var trush = document.getElementById("window4");
-// var imageviewer = document.getElementById("window5");
+var nautilus = document.getElementById("window3");
+var trush = document.getElementById("window4");
+var imageviewer = document.getElementById("window5");
 
 // window close (just hide)
 var close = document.getElementsByClassName("fa-times-circle");
@@ -30,6 +30,31 @@ firefox.addEventListener("mousedown", function(){
     init();
     firefox.classList.add("active");
 });
+$('iframe').on("load", function(){
+    $(this).contents().find("body").on('mousedown', function(){ 
+        title.innerText = "Mozilla Firefox";
+        init();
+        firefox.classList.add("active"); 
+    });
+});
+
+nautilus.addEventListener("mousedown", function(){ 
+    title.innerText = "Nautilus";
+    init();
+    nautilus.classList.add("active");
+});
+
+trush.addEventListener("mousedown", function(){ 
+    title.innerText = "Trush";
+    init();
+    trush.classList.add("active");
+});
+
+imageviewer.addEventListener("mousedown", function(){ 
+    title.innerText = "Imageviewer";
+    init();
+    imageviewer.classList.add("active");
+});
 
 desktop.addEventListener("mousedown", function(){ 
     title.innerText = "Kabuntu Desktop"; 
@@ -42,7 +67,25 @@ function init() {
     desktop.classList.remove("active");
     terminal.classList.remove("active");
     firefox.classList.remove("active");
-    // nautilus.classList.remove("active");
-    // trush.classList.remove("active");
-    // imageviewer.classList.remove("active");
+    nautilus.classList.remove("active");
+    trush.classList.remove("active");
+    imageviewer.classList.remove("active");
+}
+
+// open window (form sidebar)
+var s_search = document.getElementById("s_search");
+s_search.addEventListener("click", function(){
+    // tbc
+    console.log(this);
+});
+
+for(let i=1; i<=5; i++){
+    let sidebar_item = document.getElementById("s_" + i);
+    let window = document.getElementById("window" + i);
+    sidebar_item.addEventListener("click", function(){
+        console.log(this);
+        init();
+        $(window).show();
+        window.classList.add("active");
+    });
 }
